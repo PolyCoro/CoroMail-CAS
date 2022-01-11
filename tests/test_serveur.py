67d6038d1223bb74bcb094182a6cf47547017d27
Paul-Edouard <pauledouardg@gmail.com>
2022-01-11 11:48:10 +0100

    Test serveur : AddUser

--- tests/test_serveur.py
+++ tests/test_serveur.py
@@ -3,27 +3,10 @@ import requests
 import shlex
 import subprocess
 import time
-from user_server import UserSrv
 import sqlite3
-
-# id ip password port username
-
-conn = sqlite3.connect('database.db')
-cursor = conn.cursor()
-cursor.execute("""CREATE TABLE IF NOT EXISTS users(id int, ip TEXT, password TEXT, port int, username TEXT) """)
-conn.commit()
-
-test = UserSrv()
-
-count = 1
-
-test.addUser(1,"102.021.32.12" ,"password_karim",102,"Karim")
-test.addUser(2,"99.99.99.910"  ,"password_PE"   ,2563,"PE")
-test.addUser(3,"120.325.365.45","password_isma" ,12002,"ISMA")
-test.addUser(-1,"","ef" ,0,"ef")
-test.addUser(-1,"ef","" ,0,"ef")
-test.addUser(-1,"ef","ef" ,0,"")
-test.addUser(-1,"ef&!$","ef" ,0,"")
+import os
+import src
+from src.serveur import UserSrv
 
 class TestUserSrv(unittest.TestCase):
 
@@ -34,16 +17,99 @@ class TestUserSrv(unittest.TestCase):
 	# SrvUrl = "http://" + SrvAddr + ":" + TestPort
 
 	def test_addUser(self):
-		self.assertEqual(test.cursor.execute("""SELECT * FROM users WHERE username="Karim" """).fetchone(),(1,"102.021.32.12","password_karim",102,"Karim"))
-		self.assertEqual(test.cursor.execute("""SELECT * FROM users WHERE password="password_isma" """).fetchone(),(3,"120.325.365.45","password_isma",12002,"ISMA"))
-		self.assertEqual(test.cursor.execute("""SELECT * FROM users WHERE ip="99.99.99.910" """).fetchone(),(2,"99.99.99.910","password_PE",2563 ,"PE"))
-		self.assertEqual(test.cursor.execute("""SELECT * FROM users WHERE ip="" """).fetchone(),(-1,"","ef",0 ,"ef"))
-		self.assertEqual(test.cursor.execute("""SELECT * FROM users WHERE password="" """).fetchone(),(-1,"ef","",0 ,"ef"))
-		self.assertEqual(test.cursor.execute("""SELECT * FROM users WHERE username="" """).fetchone(),(-1,"ef","ef",0 ,""))
-		self.assertEqual(test.cursor.execute("""SELECT * FROM users WHERE ip="ef&!$" """).fetchone(),(-1,"ef&!$","ef",0 ,""))
+		# ouverture/initialisation de la base de donnee 
+		try:
+			os.remove('test.db')
+		except:
+			pass
+
+		conn = sqlite3.connect('test.db')
+		conn.row_factory = sqlite3.Row
+		c = conn.cursor()
+		c.execute("""CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, ip TEXT, password TEXT, port int, username TEXT) """)
+		
+		test = UserSrv()
+
+		res = test.addUser("102.021.32.12" ,"password_karim",102,"Karim")
+		self.assertEqual(res,True)
+		c.execute("SELECT * FROM users WHERE username='Karim'")
+		rows = c.fetchall()
+		for row in rows:
+			self.assertEqual(row['id'], 1)
+			self.assertEqual(row['ip'], "102.021.32.12")
+			self.assertEqual(row['password'], "password_karim")
+			self.assertEqual(row['port'], 102)
+			self.assertEqual(row['username'], "Karim")
+
+		res = test.addUser("99.99.99.910","password_PE",2563,"PE")
+		self.assertEqual(res,True)
+		c.execute("SELECT * FROM users WHERE id=2")
+		rows = c.fetchall()
+		for row in rows:
+			self.assertEqual(row['id'], 2)
+			self.assertEqual(row['ip'], "99.99.99.910")
+			self.assertEqual(row['password'], "password_PE")
+			self.assertEqual(row['port'], 2563)
+			self.assertEqual(row['username'], "PE")
+
+		res = test.addUser(" ","ef",0,"ef")
+		self.assertEqual(res,True)
+		c.execute("SELECT * FROM users WHERE ip=' '")
+		rows = c.fetchall()
+		for row in rows:
+			self.assertEqual(row['id'], 3)
+			self.assertEqual(row['ip'], "")
+			self.assertEqual(row['password'], "ef")
+			self.assertEqual(row['port'], 0)
+			self.assertEqual(row['username'], "ef")
+
+		res = test.addUser("ef"," ",0,"ef")
+		self.assertEqual(res,True)
+		c.execute("SELECT * FROM users WHERE password=' '")
+		rows = c.fetchall()
+		for row in rows:
+			self.assertEqual(row['id'], 3)
+			self.assertEqual(row['ip'], "")
+			self.assertEqual(row['password'], "ef")
+			self.assertEqual(row['port'], 0)
+			self.assertEqual(row['username'], "ef")
+
+		res = test.addUser("ef","ef" ,0," ")
+		self.assertEqual(res,True)
+		c.execute("SELECT * FROM users WHERE username=' '")
+		rows = c.fetchall()
+		for row in rows:
+			self.assertEqual(row['id'], 4)
+			self.assertEqual(row['ip'], "ef")
+			self.assertEqual(row['password'], "ef")
+			self.assertEqual(row['port'], 0)
+			self.assertEqual(row['username'], " ")
+		
+		res = test.addUser("ef&!$","ef" ,0," ")
+		self.assertEqual(res,True)
+		c.execute("SELECT * FROM users WHERE username=' '")
+		rows = c.fetchall()
+		for row in rows:
+			self.assertEqual(row['id'], 5)
+			self.assertEqual(row['ip'], "ef&!$")
+			self.assertEqual(row['password'], "ef")
+			self.assertEqual(row['port'], 1)
+			self.assertEqual(row['username'], " ")
+
+		res = test.addUser("","ef" ,0, "ef")
+		self.assertEqual(res,False)
+
+		res = test.addUser("ef","" ,0, "ef")
+		self.assertEqual(res,False)
+
+		res = test.addUser("ef","ef" ,0, "")
+		self.assertEqual(res,False)
+		
+		conn.commit()
+		conn.close()
 	
-	# def test_hash(self):
-    #     self.assertEqual(test.hash("example"),hash("example"))
+	# def test_hashed(self):
+    #     self.assertEqual(test.hashed("example"),hash("example"))
 	
 	# def setUp(self):
 	# 	cmd = "python user_server.py --port="+self.TestPort
